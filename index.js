d78193958a3e6f475cc0fcde21fc4e60d30dcb68
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-04-28 01:12:45 -0400

    vars in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+const { clientId, guildId, token }  = {process.env.CLIENT_ID, process.env.GUILD_ID, process.env.DISCORD_TOKEN};
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
