a509cc9544807403da14319d3a2ba547ebe7f07e
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-04-28 14:22:20 -0400

    name and description

--- commands/mod/cooldown.js
+++ commands/mod/cooldown.js
@@ -1,1 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cooldown')
+		.setDescription('Isolates user in a cooldown channel'),
+	async execute(interaction) {
+		await interaction.reply('');
+	},
+};
