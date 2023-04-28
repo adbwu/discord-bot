45183449abc263ce298f7c4ca7e895418774daef
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-04-28 16:35:51 -0400

    cooldown feature for commands

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+  //cooldown: seconds users has to wait before using command again
+  cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
