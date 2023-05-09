0f037e852037594e9f908dce891ab5ebc636abe2
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-05-09 17:57:25 -0400

    embed builder

--- commands/menus/role-menu.js
+++ commands/menus/role-menu.js
@@ -1,7 +1,15 @@
-const {SlashCommandBuilder} = requie('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = requie('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('role-menu')
-    .setDescription('Responds with role selction menu.')
+    .setDescription('Responds with role selction menu.'),
+  async execute(interaction) {
+    const roleMenuEmbed = new EmbedBuilder()
+      .setColor(0xff69b4)
+      .setTitle(`Role Menu`)
+      .setDescription(`Select the buttons below to choose your roles.`)
+      
+    await interaction.reply('')
+  },
 }
