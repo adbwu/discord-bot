6eabea9cbcf39d83bae44d0ef0ad32dc8527b6f7
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-05-09 17:36:27 -0400

    name and desc

--- commands/menus/role-menu.js
+++ commands/menus/role-menu.js
@@ -1,5 +1,7 @@
 const {SlashCommandBuilder} = requie('discord.js');
 
 module.exports = {
-  
+  data: new SlashCommandBuilder()
+    .setName('role-menu')
+    .setDescription('Responds with role selction menu.')
 }
